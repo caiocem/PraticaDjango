45852d0abda4ed52ba937534e7d890db5c9fbce2
caiohenrici <caiohenrici@gmail.com>
2021-04-07 23:54:19 -0300

    template horas

---
+++ aprop/templates/horas.js
@@ -0,0 +1,11 @@
+function vuscaColabs(){
+    var disp = document.getElementById('resultadobusca')
+    if(disp.style.display == '' || disp.style.display == 'none'){
+      disp.style.display = 'block';
+    }
+}
+
+function fechabusca(){
+    var disp = document.getElementById('resultadobusca')
+    disp.style.display = 'none';
+}
